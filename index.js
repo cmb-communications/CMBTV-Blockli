df3801fa5dcfe2954fb8e6f8c01252c2d3bd9047
cmb-communications <112956016+cmb-communications@users.noreply.github.com>
2022-12-04 15:23:48 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,13 @@ import { initialize } from "@blocklienterprise/blockli";
 import config from "@src/build_config.json";
 
 export const applyCustomCode = async (externalCodeSetup) => {
-  
+  const blockli_config = {
+    license: "7DL0IORJK3QUXRF", // Enter your 15 digit Blockli App Kit license key here. See your account dashboard at https://blockli.dev/dashboard
+    app_id: config.app_id,
+    code: externalCodeSetup,
+    website: "https://partners.cardiometabology.com", // Enter your app domain here with NO trailing slash.
+    // NOTE: If you have a staging site, please use a different Git branch for your test app and COPY this entire code to the test branch and enter the staging site domain.
+  };
+
+  await initialize(blockli_config);
 };
